780a4a9e78a24582f3a6a38c21e1e64af288bc22
Genita98 <70575448+Genita98@users.noreply.github.com>
2020-12-13 15:31:08 +0100

    initial commit

---
+++ HeightAlgorithm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HeightAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // i popullojme vlerat e tree-s
+            Tree tree = new Tree();
+
+            tree.root = new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            tree.root.left.right = new Node(5);
+
+            Console.WriteLine("Lartesia (height) e pemes eshte : " + tree.maxDepth(tree.root));
+            Console.ReadLine();
+
+        }
+    }
+
+
+}
---
+++ HeightAlgorithm/Tree.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+  
+// e krijojme fillimisht nje binary tree node (nyje)
+public class Node  
+{ 
+    public int data; 
+    public Node left, right; 
+  
+    public Node(int item)  
+    { 
+        data = item; 
+        left = right = null; 
+    } 
+}
+
+public class Tree
+{
+    public Node root;
+
+    
+    // ketu e krijojme nje metode e cila gjen thellesine maksimale te Depth ne menyre qe te gjindet lartesia e vargut
+   public int maxDepth(Node node)
+    {
+        if (node == null)
+            return 0;
+        else
+        {
+           // nese nuk eshte node = null atehere e llogarisim thellesine te secilit subtree 
+            int leftDepth = maxDepth(node.left);
+            int rightDepth = maxDepth(node.right);
+
+            //pasi e llogarisim depth maksimal te seciles subtree ketu e perdorim depth i cili eshte me i larte 
+            if (leftDepth > rightDepth)
+                return (leftDepth + 1);
+            else
+                return (rightDepth + 1);
+        }
+    }
+}
